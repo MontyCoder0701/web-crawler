70e8012f082f13f6710e5202ca1f974ad7c185cc
Soo <soojlee0106@einsis.com>
2022-12-11 22:15:08 +0900

    Parse with split

--- static/static.py
+++ static/static.py
@@ -1,5 +1,20 @@
-# s = "제 생일은 9월 입니다"
-# pos = s.find("생일은 ")
-# pos += 4
+import requests as req
 
-# print(s[pos])
+# s = "제 생일은 10월 입니다"
+# # pos = s.find("생일은 ")
+# # pos += 4
+
+# # print(s[pos])
+
+# # arr = s.split("생일은 ")
+# # s = arr[1]
+
+# # print(s.split("월 ")[0])
+# bd = s.split("생일은 ")[1].split("월")[0]
+# print(bd)
+
+res = req.get(
+    "https://finance.naver.com/marketindex/")
+html = res.text
+s = html.split('<span class="value">')[1].split('</span>')[0]
+print(s)
