670ad21aa8c56921584fb0ce7f55ad0e12d13de9
Soo <soojlee0106@einsis.com>
2022-12-12 20:34:57 +0900

    practice css selector

--- static/main.py
+++ static/main.py
@@ -2,7 +2,7 @@ import requests as req
 import re
 from bs4 import BeautifulSoup as BS
 
-# Using re
+# Using regex
 url = "https://finance.naver.com/marketindex/"
 res = req.get(url)
 body = res.text
@@ -33,7 +33,6 @@ soup = BS(res.text, "html.parser")
 tds = soup.find_all("td")
 
 names = []
-
 for td in tds:
     if len(td.find_all("a")) == 0:
         continue
@@ -43,6 +42,15 @@ prices = []
 for td in tds:
     if "class" in td.attrs and "sale" in td.attrs["class"]:
         prices.append(td.get_text(strip=True))
+
+# Using CSS selector
+names = []
+for td in soup.select("td.tit"):
+    names.append(td.get_text(strip=True))
+
+prices = []
+for td in soup.select("td.sale"):
+    names.append(td.get_text(strip=True))
 
 print(names)
 print(prices)
--- static/static_prac.py
+++ static/static_prac.py
@@ -42,7 +42,28 @@ from bs4 import BeautifulSoup as BS
 # print(res.text)
 
 # beautifulsoup 연습
-url = "https://naver.com"
-res = req.get(url)
-soup = BS(res.text, "html.parser")
-print(soup.title)
+# url = "https://naver.com"
+# res = req.get(url)
+# soup = BS(res.text, "html.parser")
+# print(soup.title)
+
+# url = "https://search.shopping.naver.com/search/all?query=%EC%95%84%EC%9D%B4%ED %8F%B0%20%EC%BC%80%EC%9D%B4%EC%8A%A4&cat_id=&frm=NVSHATC"
+# res = req.get(url)
+# soup = BS(res.text, "html.parser")
+
+# arr = soup.select("ul.list_basis div>a:first-child[title]")
+# for a in arr:
+#     print(a.get_text(strip=True))
+
+# CSS selector 연습
+# url = "https://www.coupang.com/np/search?component=&q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user"
+# res = req.get(url, headers={
+#               "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"})
+# soup = BS(res.text, "html.parser")
+
+# for desc in soup.select("div.descriptions-inner"):
+#     ads = desc.select("span.ad-badge")
+#     if len(ads) == 0:
+#         print(desc.select("div.name")[0].get_text(strip=True))
+#     else:
+#         print("광고")
