1ae85838bc95fc2356ea6391280efabefa94e532
unknown <soojlee0106@naver.com>
2022-12-22 09:03:40 +0900

    Crawl top three articles

--- static/gui.py
+++ static/gui.py
@@ -7,6 +7,13 @@ import time
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 
 st.set_page_config(layout="wide")
 
@@ -23,6 +30,67 @@ components.html(
     """,
     height=120,
 )
+
+components.html(
+    """
+    <div style= "color: #000040; font-weight: bold; text-align: left; font-size: 20px; font-family: Trebuchet MS" >
+    Current Top Articles
+    </div>
+    """,
+    height=30,
+)
+
+url = "https://www.timeanddate.com/worldclock/south-korea/seoul"
+res = req.get(url)
+soup = BS(res.text, "html.parser")
+
+for stat in soup.select("div.bk-focus__qlook"):
+    now_time = stat.select("span.h1")[0].get_text(strip=True)
+    with st.spinner(text="Live Crawling..."):
+        time.sleep(5)
+        st.success("Crawling complete. Live Crawled Time: " + now_time)
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_argument("window-size=1000,1000")
+options.add_argument("lang=en-GB")
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument("no-sandbox")
+
+chrome = webdriver.Chrome(service=Service(
+    ChromeDriverManager().install()), options=options)
+chrome.get("https://google.com")
+
+wait = WebDriverWait(chrome, 10)
+
+
+def find(wait, css_selector):
+    return wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
+
+
+search = find(wait, "body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input")
+search.send_keys("investment\n")
+
+button = find(
+    wait, "#hdtb-msb > div:nth-child(1) > div > div:nth-child(2) > a > span")
+button.click()
+
+headline_1 = wait.until(EC.presence_of_element_located(
+    (By.CSS_SELECTOR, "#rso > div > div > div:nth-child(1) > div > div > a > div > div.iRPxbe > div.mCBkyc.ynAwRc.MBeuO.nDgy9d")))
+st.write(headline_1.text)
+
+headline_2 = wait.until(EC.presence_of_element_located(
+    (By.CSS_SELECTOR, "#rso > div > div > div:nth-child(2) > div > div > a > div > div.iRPxbe > div.mCBkyc.ynAwRc.MBeuO.nDgy9d")))
+st.write(headline_2.text)
+
+headline_3 = wait.until(EC.presence_of_element_located(
+    (By.CSS_SELECTOR, "#rso > div > div > div:nth-child(3) > div > div > a > div > div.iRPxbe > div.mCBkyc.ynAwRc.MBeuO.nDgy9d")))
+st.write(headline_3.text)
+
+chrome.close()
+
 
 components.html(
     """
@@ -88,7 +156,7 @@ with col1:
         list_one.append(title)
         price = tr.select("td.number:nth-child(4)")[0].get_text(strip=True)
         change = tr.select("td.number:nth-child(6)")[0].get_text(strip=True)
-        if len(list_one) >= 11:
+        if len(list_one) >= 8:
             break
         st.write(title, price, change)
 
@@ -105,7 +173,7 @@ with col2:
         list_two.append(title)
         price = stat.select("div.YMlKec")[0].get_text(strip=True)
         change = stat.select("div.JwB6zf")[0].get_text(strip=True)
-        if len(list_two) >= 11:
+        if len(list_two) >= 7:
             break
         st.write(title, price, change)
 
